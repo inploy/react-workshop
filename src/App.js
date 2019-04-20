8eb179b4ea8688f010ae51fcbea8ef4eceddd93b
LAPTOP-6J9QVRCC\Ploy <apisada.cha@gmail.com>
2019-04-21 00:44:39 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -2,25 +2,64 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+  
+<div className="App">
+        <div className="app-left"><br />
+          <img className="image"
+          alt = "profile"
+          src="https://scontent.fbkk22-2.fna.fbcdn.net/v/t1.0-9/14212780_1160979357296084_6783723684663381606_n.jpg?_nc_cat=106&_nc_ht=scontent.fbkk22-2.fna&oh=d7fc2d531d629f44e5803b5ad6287d21&oe=5D31C08D"
+         />
+      
+          <h1>Apisada Chaitamol</h1>
+          <h3>Student ,Faculty Applied Mathematics</h3>
+          <h4> King Mongkut's Institute of Technology Ladkrabang</h4>
+        <div classname="text">
+          <label>Email   : apisada.cha@gmail.com</label><br></br>
+          <label>Address : Saimai,Bangkok</label>
+          
+          <p><h2>- Technical Skills</h2></p> 
+          <li>Adobe Photoshop</li>
+          <li>Java Language</li>
+          <li>Visual Basic</li>
+          <li>HTML&CSS</li>
+          <li>Database Oracle,My SQL</li>
+          <li>Java SpringBoots</li>
+          <li>AngularJS</li>
+          <li>Bootstrap</li>
+       </div>
+       <br />
+       </div>
+       <div class="vl"></div>
+       <div className="app-right">
+         <h2>- Education</h2>
+         <h4 ><b>King Mongkut's Institute of Technology Ladkrabang  2015 - Current</b></h4>
+          <p>Faculty of Science in Applied Mathematics. GPA : 3.23</p>
+        <h4><b>Saipanya School Under the Royal Patronage of her Majesty the Queen</b></h4>
+        <p>Graduated high school with GPA : 3.02</p><br />
+        <hr className="new4"></hr><br />
+        <h2>- Work Experience</h2>
+         <h3 ><b>Internship (6 months)</b></h3>
+        <h4><b>GENERAL ELECTRONIC COMMERCE SERVICES CO.,LTD. , Bangkok, Thailand</b></h4>
+        <p>FullStack - Java Springboot , AngularJS , My SQL</p>
+        <br />
+        <hr className="new4"></hr><br />
+        <h2>- Objective</h2>
+        <p>By obtaining a new challenging experience and be in the part of organization, which offers a professional working environment, help me to achieve my goals as well as organizational goals.</p>
+
+       </div>
+    
+       
       </div>
+
+
+
+
+
+
     );
   }
 }
